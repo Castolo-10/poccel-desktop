f145a21eee9276b44804ec38ec2c47b57a60ccf5
fernandolp98 <fernando.lparra@alumnos.udg.mx>
2019-03-02 19:45:02 -0600

    Cambios en interfaz y funciones de clientes

---
+++ Poccel-desktop/Poccel-desktop/Venta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poccel_desktop
+{
+    class Venta
+    {
+        public int idVenta { get; set; }
+        public int idSucursal { get; set; }
+        public int idEmpleado { get; set; }
+        public int idCliente { get; set; }
+        public DateTime fecha { get; set; }
+        public DateTime hora { get; set; }
+    }
+}
